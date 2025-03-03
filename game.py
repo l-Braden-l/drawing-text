1652e00e6b3c864c38eb1eb2d589411a4f35321d
braden <leachbr787@northwested.org>
2025-03-03 13:38:15 -0500

    Update game.py

--- game.py
+++ game.py
@@ -19,6 +19,22 @@ def handle_events ():
              return False
     return True
 
+
+
+txt_font = pygame.font.SysFont("Ariel", 30, bold = True, italic = True)
+#-------------------------------------------------
+
+def draw_text(text,font,text_col,x,y):
+   render_txt = font.render(text, True, text_col)
+   screen.blit(render_txt, (x,y))
+
+
+
+
+
+
+
+#--------------------------------------------------
 def main():
    screen = init_game()
    clock = pygame.time.Clock() # Initialize the clock here
@@ -26,6 +42,9 @@ def main():
    while running:
       running = handle_events()
       screen.fill(config.WHITE) # Use color from config
+
+      draw_text("Hello World",txt_font, (0,0,0), 220, 150)
+
       pygame.display.flip()
 
       # -- Limit the frame rate to the specified frames per second (FPS) -- #
